060c05364c4e8ff627ddcdc2e77cb70f794d4eaa
Erfan Gharib <erfangharib5@gmail.com>
2024-06-09 12:04:32 +0330

    feat(client): implement offlineMode banner

---
+++ client/src/components/OfflineModeBanner.vue
@@ -0,0 +1,14 @@
+<script setup lang='ts'>
+import { offlineModeStore } from '../store/offlineMode';
+
+const offlineMode = offlineModeStore();
+</script>
+
+<template>
+    <div
+        v-if='offlineMode.isOfflineMode'
+        class='w-full bg-red-900 text-red-200 capitalize opacity-80 p-2 f-center text-xs'
+    >
+        Server is Unavailable. offline mode turned on
+    </div>
+</template>
